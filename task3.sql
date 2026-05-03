2361bfff664c961b647c42247681d7045e601175
ifirdavs <fismoilov20@gmail.com>
2026-05-03 12:17:28 +0500

    task4.sql complete

---
+++ task3.sql
@@ -0,0 +1,27 @@
+-- Task 3 - Database: world
+
+
+WITH capital_population_share AS (
+    SELECT
+        c.Code AS CountryCode,
+        c.Name AS CountryName,
+        capital.Name AS CapitalName,
+        c.Population AS CountryPopulation,
+        capital.Population AS CapitalPopulation,
+        100.0 * capital.Population / c.Population AS CapitalPopulationPercentage
+    FROM country AS c
+    INNER JOIN city AS capital
+        ON capital.ID = c.Capital
+    WHERE c.Population > 0
+      AND c.Capital IS NOT NULL
+)
+SELECT
+    CountryCode,
+    CountryName,
+    CapitalName,
+    CountryPopulation,
+    CapitalPopulation,
+    ROUND(CapitalPopulationPercentage, 4) AS CapitalPopulationPercentage
+FROM capital_population_share
+ORDER BY CapitalPopulationPercentage ASC, CountryName ASC
+LIMIT 10;
